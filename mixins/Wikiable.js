f2be24af4012ae946024ff5a898e577684fcb125
kanaxz <kanaxstratz@gmail.com>
2024-09-07 16:29:54 +0200

    1.15.0

--- mixins/Wikiable.js
+++ mixins/Wikiable.js
@@ -1,6 +1,6 @@
 const mixer = require('sools-core/mixer')
 const Propertiable = require('sools-core/mixins/Propertiable')
-const Markdown = require('sools-modeling/types/Markdown')
+const { Markdown } = require('sools-modeling/types')
 
 module.exports = mixer.mixin([Propertiable], (base) => {
   return class Wikiable extends base {
